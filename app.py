849ecf1c864dd2f4379430b23061ef859501ec77
Varadharajan Damotharan <vdamotharan@microsoft.com>
2026-04-19 07:36:48 +0530

    Update app version to 4.0 in home route and index.html title

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", app_name="Flask Elastic Beanstalk Demo Version 3.0")
+    return render_template("index.html", app_name="Flask Elastic Beanstalk Demo Version 4.0")
 
 
 @app.route("/health")
