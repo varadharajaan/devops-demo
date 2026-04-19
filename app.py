7ed04fa737610b77f87c87a0f3c04ac29f466c3b
Varadharajan Damotharan <vdamotharan@microsoft.com>
2026-04-19 07:32:20 +0530

    Update app version to 3.0 in home route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", app_name="Flask Elastic Beanstalk Demo Version 2.0")
+    return render_template("index.html", app_name="Flask Elastic Beanstalk Demo Version 3.0")
 
 
 @app.route("/health")
